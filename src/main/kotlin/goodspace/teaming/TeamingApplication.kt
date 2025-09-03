541627a0e510b8484393622a981d5cbafc957eb0
Joon Kim <kjoon418@naver.com>
2025-09-03 21:17:36 +0900

    [feat] 프로젝트 생성

---
+++ src/main/kotlin/goodspace/teaming/TeamingApplication.kt
@@ -0,0 +1,11 @@
+package goodspace.teaming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamingApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamingApplication>(*args)
+}
---
+++ src/test/kotlin/goodspace/teaming/TeamingApplicationTests.kt
@@ -0,0 +1,13 @@
+package goodspace.teaming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
