fe643c354e8832beef8455cdb831f4d8e21ea195
Joon Kim <kjoon418@naver.com>
2025-09-04 15:00:35 +0900

    [fix] 어플리케이션에 JpaAuditing 허용

--- src/main/kotlin/goodspace/teaming/TeamingApplication.kt
+++ src/main/kotlin/goodspace/teaming/TeamingApplication.kt
@@ -2,8 +2,10 @@ package goodspace.teaming
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class TeamingApplication
 
 fun main(args: Array<String>) {
