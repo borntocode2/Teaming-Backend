221a2582d32bdff22aff6af33383823944aa1efa
ralph <sanghwaj222@gmail.com>
2025-09-11 14:50:15 +0900

    [feat/#7] create PaymentVeriftRespondDto

---
+++ src/main/kotlin/goodspace/teaming/payment/dto/PaymentVerifyRespondDto.kt
@@ -0,0 +1,13 @@
+package goodspace.teaming.payment.dto
+
+class PaymentVerifyRespondDto (
+    val authResultCode: String,
+    val authResultMsg: String,
+    val tid: String,
+    val clientId :String,
+    val orderId: String,
+    val amount: String,
+    val mallReserved: String,
+    val authToken: String,
+    val signature: String
+)
