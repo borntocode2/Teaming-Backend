a98188529703ead9119ca95c7f534d8d9ff0e230
Joon Kim <kjoon418@naver.com>
2025-09-18 16:33:51 +0900

    [feat/#26] 테스트용 관리자 계정을 2개 생성하도록 수정

--- src/main/kotlin/goodspace/teaming/init/AdminUserInitializer.kt
+++ src/main/kotlin/goodspace/teaming/init/AdminUserInitializer.kt
@@ -14,16 +14,34 @@ import org.springframework.transaction.annotation.Transactional
 @Component
 class AdminUserInitializer(
     private val userRepository: UserRepository,
-    @Value("\${admin.email:test@test.com}")
-    private val email: String,
-    @Value("\${admin.password:test}")
-    private val password: String,
-    @Value("\${admin.name:Teaming Admin}")
-    private val name: String
+    @Value("\${admin.email1:test1@test.com}")
+    private val email1: String,
+    @Value("\${admin.password1:test1}")
+    private val password1: String,
+    @Value("\${admin.name1:Teaming Admin A}")
+    private val name1: String,
+    @Value("\${admin.email2:test2@test.com}")
+    private val email2: String,
+    @Value("\${admin.password2:test2}")
+    private val password2: String,
+    @Value("\${admin.name2:Teaming Admin B}")
+    private val name2: String
 ) {
     @Transactional
     @EventListener(ApplicationReadyEvent::class)
     fun initialize() {
+        val adminUser1 = createAdminUser(email1, password1, name1)
+        val adminUser2 = createAdminUser(email2, password2, name2)
+
+        saveIfNotExist(adminUser1)
+        saveIfNotExist(adminUser2)
+    }
+
+    private fun createAdminUser(
+        email: String,
+        password: String,
+        name: String
+    ): User {
         val user = TeamingUser(
             email = email,
             password = password,
@@ -40,7 +58,7 @@ class AdminUserInitializer(
         )
         user.addRole(userRole, adminRole)
 
-        saveIfNotExist(user)
+        return user
     }
 
     private fun saveIfNotExist(user: User) {
