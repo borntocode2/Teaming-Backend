25c7be2fcbed37952b9b0236f9f755b4ba2f09e6
ralph <sanghwaj222@gmail.com>
2025-09-21 15:42:52 +0900

    not null handling in naverAuthService

--- src/main/kotlin/goodspace/teaming/authorization/dto/NaverUserInfoResponseDto.kt
+++ src/main/kotlin/goodspace/teaming/authorization/dto/NaverUserInfoResponseDto.kt
@@ -4,11 +4,11 @@ class NaverUserInfoResponseDto (
     val resultCode: String? = null,
     val message: String? = null,
 
-    val response: Response
+    val response: Response,
 ) {
     data class Response (
         val id: String,
-        val email: String,
-        val nickname: String
+        val email: String?,
+        val nickname: String?
     )
 }
