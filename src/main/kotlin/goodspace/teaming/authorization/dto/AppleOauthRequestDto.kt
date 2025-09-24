15c27265c6586898649e9e1c2623f8662b9b3308
Kim, Joon <kjoon418@naver.com>
2025-09-24 22:29:13 +0900

    [fix/#96] Apple Oauth 요청 파라미터에 `codeVerifier` 추가

--- src/main/kotlin/goodspace/teaming/authorization/dto/AppleOauthRequestDto.kt
+++ src/main/kotlin/goodspace/teaming/authorization/dto/AppleOauthRequestDto.kt
@@ -3,5 +3,6 @@ package goodspace.teaming.authorization.dto
 data class AppleOauthRequestDto(
     val code: String,
     val redirectUri: String,
-    val name: String? = null
+    val name: String? = null,
+    val codeVerifier: String
 )
