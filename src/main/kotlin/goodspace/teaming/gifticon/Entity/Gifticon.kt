cdfcf6c79a0d9985a1f1be61fa7298726101d597
ralph <sanghwaj222@gmail.com>
2025-09-24 15:38:14 +0900

    gifticon annotation

--- src/main/kotlin/goodspace/teaming/gifticon/Entity/Gifticon.kt
+++ src/main/kotlin/goodspace/teaming/gifticon/Entity/Gifticon.kt
@@ -1,9 +1,11 @@
 package goodspace.teaming.gifticon.Entity
 
+import goodspace.teaming.global.entity.BaseEntity
+import jakarta.persistence.MappedSuperclass
 import java.time.LocalDateTime
 
-
+@MappedSuperclass
 abstract class Gifticon (
     protected val code: String,
     protected val expirationDate: LocalDateTime
-)
+): BaseEntity()
--- src/main/kotlin/goodspace/teaming/gifticon/Entity/StandardGifticon.kt
+++ src/main/kotlin/goodspace/teaming/gifticon/Entity/StandardGifticon.kt
@@ -1,9 +1,18 @@
 package goodspace.teaming.gifticon.Entity
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import java.time.LocalDateTime
 
+@Entity
 class StandardGifticon (
     code: String,
     expirationDate: LocalDateTime,
     grade: Grade
-): Gifticon(code, expirationDate)
+): Gifticon(code, expirationDate){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long ?= null
+}
