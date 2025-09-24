99ac4d14dd1753aea1e9dba87032d38b95d5d97b
ralph <sanghwaj222@gmail.com>
2025-09-24 19:06:35 +0900

    [feat/#79] add SQLRestriction annotation in Gifticon

--- src/main/kotlin/goodspace/teaming/gifticon/Entity/Gifticon.kt
+++ src/main/kotlin/goodspace/teaming/gifticon/Entity/Gifticon.kt
@@ -3,10 +3,12 @@ package goodspace.teaming.gifticon.Entity
 import goodspace.teaming.global.entity.BaseEntity
 import jakarta.persistence.*
 import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
 import java.time.LocalDateTime
 
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @SQLDelete(sql = "UPDATE `assigned_member` SET deleted = true, deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted = false")
 @Entity
 @DiscriminatorColumn(name = "gifticon_type")
 abstract class Gifticon (
