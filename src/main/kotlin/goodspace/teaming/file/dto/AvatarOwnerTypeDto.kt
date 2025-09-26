4a2cd40d2e205827f42769e4c7f8aaef489ee753
Kim, Joon <kjoon418@naver.com>
2025-09-26 20:04:58 +0900

    [chore] 아바타 API의 요청 파라미터에 Room ID 추가

--- src/main/kotlin/goodspace/teaming/file/dto/AvatarOwnerTypeDto.kt
+++ src/main/kotlin/goodspace/teaming/file/dto/AvatarOwnerTypeDto.kt
@@ -3,5 +3,6 @@ package goodspace.teaming.file.dto
 import goodspace.teaming.file.domain.AvatarOwnerType
 
 data class AvatarOwnerTypeDto(
-    val ownerType: AvatarOwnerType
+    val ownerType: AvatarOwnerType,
+    val roomId: Long? = null
 )
