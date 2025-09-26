11248abeeb6ffc685a2e36e93cf883282b5544d3
Kim, Joon <kjoon418@naver.com>
2025-09-26 17:51:51 +0900

    [fix/#124] 아바타 API 요청으로 OwnerType 추가

---
+++ src/main/kotlin/goodspace/teaming/file/dto/AvatarOwnerTypeDto.kt
@@ -0,0 +1,7 @@
+package goodspace.teaming.file.dto
+
+import goodspace.teaming.file.domain.AvatarOwnerType
+
+data class AvatarOwnerTypeDto(
+    val ownerType: AvatarOwnerType
+)
--- src/main/kotlin/goodspace/teaming/file/service/AvatarService.kt
+++ src/main/kotlin/goodspace/teaming/file/service/AvatarService.kt
@@ -1,28 +1,25 @@
 package goodspace.teaming.file.service
 
-import goodspace.teaming.file.domain.AvatarOwnerType
 import goodspace.teaming.file.dto.*
 
 interface AvatarService {
     fun intent(
-        ownerType: AvatarOwnerType,
         ownerId: Long,
-        request: AvatarUploadIntentRequestDto
+        requestDto: AvatarUploadIntentRequestDto
     ): AvatarUploadIntentResponseDto
 
     fun complete(
-        ownerType: AvatarOwnerType,
         ownerId: Long,
-        request: AvatarUploadCompleteRequestDto
+        requestDto: AvatarUploadCompleteRequestDto
     ): AvatarUploadCompleteResponseDto
 
     fun issueViewUrl(
-        ownerType: AvatarOwnerType,
-        ownerId: Long
+        ownerId: Long,
+        ownerTypeDto: AvatarOwnerTypeDto
     ): AvatarUrlResponseDto
 
     fun delete(
-        ownerType: AvatarOwnerType,
-        ownerId: Long
+        ownerId: Long,
+        ownerTypeDto: AvatarOwnerTypeDto
     )
 }
