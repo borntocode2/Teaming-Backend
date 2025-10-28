1a3e66c5edfbb5d0d29d68322b7cd4e596a0bc62
Kim, Joon <kjoon418@naver.com>
2025-10-28 12:52:37 +0900

    chore: 벌칙 스케줄러 실행 주기 변경 및 로거 수정

--- src/main/kotlin/goodspace/teaming/punishment/scheduler/PunishmentScheduler.kt
+++ src/main/kotlin/goodspace/teaming/punishment/scheduler/PunishmentScheduler.kt
@@ -1,9 +1,9 @@
 package goodspace.teaming.punishment.scheduler
 
 import goodspace.teaming.assignment.repository.AssignmentRepository
-import goodspace.teaming.global.entity.aissgnment.Assignment
 import goodspace.teaming.global.entity.aissgnment.AssignmentStatus
 import goodspace.teaming.punishment.service.PunishmentService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,16 +14,18 @@ class PunishmentScheduler (
     private val assignmentRepository: AssignmentRepository,
     private val punishmentService: PunishmentService
 ) {
-    @Scheduled(fixedRate = 1000) //TODO: 60분마다 실행
+    private val log = LoggerFactory.getLogger(PunishmentScheduler::class.java)
+
+    @Scheduled(fixedRate = 3600000)
     @Transactional
     fun checkAssignments() {
-        println("PunishmentScheduler 실행됨 at ${Instant.now()}")
+        log.info("PunishmentScheduler 실행됨 at ${Instant.now()}")
         val now = Instant.now()
-        println("Scheduler now = $now")
+        log.info("Scheduler now = $now")
         val expiredAssignments = assignmentRepository
             .findByDueBeforeAndStatusNotAndPunishedFalse(now, AssignmentStatus.COMPLETE)
 
-        println("expiredAssignments.size = ${expiredAssignments.size}")
+        log.info("expiredAssignments.size = ${expiredAssignments.size}")
 
         expiredAssignments.forEach { assignment ->
             punishmentService.applyPunishment(assignment)
