c9e3f87460f098075c3bc2881b0c826f607df88d
ralph <sanghwaj222@gmail.com>
2025-10-28 14:42:45 +0900

    delete get method in gifticon admin

--- src/main/kotlin/goodspace/teaming/admin/controller/GifticonAdminController.kt
+++ src/main/kotlin/goodspace/teaming/admin/controller/GifticonAdminController.kt
@@ -31,17 +31,4 @@ class GifticonAdminController(
 
         return ResponseEntity("save success", HttpStatus.OK)
     }
-
-    @GetMapping
-    @Operation(
-        summary = "기프티콘 조회",
-        description = "유저 이메일로 해당 유저의 기프티콘을 조회합니다."
-    )
-    fun getGifticon(@RequestParam email: String): ResponseEntity<List<GifticonResponseDto>>{
-        val dtos: List<GifticonResponseDto> = gifticonService.getGifticonsByUserEmail(email)
-
-
-        return ResponseEntity.ok(dtos)
-    }
-
 }
