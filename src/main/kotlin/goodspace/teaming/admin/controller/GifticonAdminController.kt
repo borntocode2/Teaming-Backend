015f46e9fb0d2eadaba6b5343fdb91a64677ccfb
ralph <sanghwaj222@gmail.com>
2025-10-28 13:36:38 +0900

    gifticon api uri fix

---
+++ src/main/kotlin/goodspace/teaming/admin/controller/GifticonAdminController.kt
@@ -0,0 +1,47 @@
+package goodspace.teaming.admin.controller
+
+import goodspace.teaming.gifticon.dto.GifticonRequestDto
+import goodspace.teaming.gifticon.dto.GifticonResponseDto
+import goodspace.teaming.gifticon.service.GifticonService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RequestMapping("/admin/gifticon")
+@RestController
+@Tag(
+    name = "관리자 API"
+)
+class GifticonAdminController(
+    private val gifticonService: GifticonService) {
+
+    @PostMapping
+    @Operation(
+        summary = "기프티콘 저장",
+        description = "기프티콘 코드, 기프티콘 만료기한 ex)\"20250925\", 기프티콘 등급(\"BASIC\"\",STANDARD\",\"ELITE\")"
+    )
+    fun saveGifticon(@RequestBody gifticonRequestDto: GifticonRequestDto): ResponseEntity<String> {
+        gifticonService.saveGifticon(
+            code = gifticonRequestDto.code,
+            expiration = gifticonRequestDto.expirationDateStr,
+            grade = gifticonRequestDto.grade
+        )
+
+        return ResponseEntity("save success", HttpStatus.OK)
+    }
+
+    @GetMapping
+    @Operation(
+        summary = "기프티콘 조회",
+        description = "유저 이메일로 해당 유저의 기프티콘을 조회합니다."
+    )
+    fun getGifticon(@RequestParam email: String): ResponseEntity<List<GifticonResponseDto>>{
+        val dtos: List<GifticonResponseDto> = gifticonService.getGifticonsByUserEmail(email)
+
+
+        return ResponseEntity.ok(dtos)
+    }
+
+}
