7a127324f9fae00025798a28c2a68998988a6c14
Kim, Joon <kjoon418@naver.com>
2025-11-17 13:45:49 +0900

    build: KotlinLogging에 대한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,6 +87,9 @@ dependencies {
     // Coroutine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1")
+
+    // KotlinLogging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 }
 
 kotlin {
